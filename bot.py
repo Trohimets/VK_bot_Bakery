3de5892bf9073297e4aaefc9f24c3d2321841fd3
Константин Трохимец <katrohimets@mail.ru>
2022-08-31 16:39:52 +0300

    add db

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
 from dotenv import load_dotenv
 
@@ -18,25 +19,45 @@ def main():
 
     longpoll = VkBotLongPoll(vk_session, GROUP_ID)
     vk = vk_session.get_api()
+
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Привет', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_button('Клавиатура', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_line()
+    keyboard.add_location_button()
+    keyboard.add_line()
+    keyboard.add_vkpay_button(hash="action=transfer-to-group&group_id=еще_раз_ID_группы")
     for event in longpoll.listen():
 
+
         if event.type == VkBotEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
+            if event.obj.message['text'] == 'start':
+                if event.from_chat:
+                    vk.messages.send(
+                    keyboard = keyboard.get_keyboard(),
+                    key = ('0128675ab00d433e951e774a78363d77ed87cd92'),          #ВСТАВИТЬ ПАРАМЕТРЫ
+                    server = ('im.vk.com/nim215636611'),
+                    ts=('1758660054'),
+                    random_id = get_random_id(),
+              	    message='Держи',
+             	    chat_id = event.chat_id
+            	    )
+            else:
+                print('Новое сообщение:')
+                print('Для меня от: ', end='')
+                dt = event.obj.message['from_id']
+                print(dt)
 
-            print('Для меня от: ', end='')
-            dt = event.obj.message['from_id']
-            print(dt)
+                print('Текст:', event.obj.message['text'])
+                print()
 
-            print('Текст:', event.obj.message['text'])
-            print()
+                vk.messages.send(
+                    user_id=event.obj.message['from_id'],
 
-            vk.messages.send(
-                user_id=event.obj.message['from_id'],
-
-                random_id=get_random_id(),
-                message=("Новое сообщение - " + event.obj.message['text'])
-            )
-            print('ok')
+                    random_id=get_random_id(),
+                    message=("Новое сообщение - " + event.obj.message['text'])
+                )
+                print('ok')
 
         elif event.type == VkBotEventType.MESSAGE_REPLY:
             print('Новое сообщение:')
--- db.py
+++ db.py
@@ -1,6 +1,8 @@
 import sqlite3
 from sqlite3 import Error
 
+
+#Подключение к БД
 def create_connection(path):
     connection = None
     try:
@@ -11,5 +13,79 @@ def create_connection(path):
 
     return connection
 
+connection = create_connection("D:\dev\VK_bot_Bakery\db.sqlite")
 
- 
+#Функция создания таблицы в БД
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+#Запрос на создание таблицы Категории
+create_categories_table = """
+CREATE TABLE IF NOT EXISTS categories (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type TEXT NOT NULL
+);
+""" 
+#Запрос на создание записей в таблице Категории
+create_categories = """
+INSERT INTO
+  categories (type)
+VALUES
+  ('Торты'),
+  ('Выпечка'),
+  ('Хлеб');
+"""
+#Запрос на создание таблицы Продукты
+create_products_table = """
+CREATE TABLE IF NOT EXISTS products (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  image TEXT NOT NULL,
+  category INT,
+  FOREIGN KEY (category) REFERENCES Categories (id) 
+        ON DELETE RESTRICT ON UPDATE CASCADE
+);
+""" 
+#Запрос на создание записей в таблице Продукты
+create_products = """
+INSERT INTO
+  Products (name, description, image, category)
+VALUES
+  ("Клубничный пирог", "Свежие ягоды", 'link to image', 2),
+  ("Малиновый пай", "Диетическое тесто", 'link to image', 2),
+  ("Осетинский пирог", "С бараниной", 'link to image', 2),
+  ("Торт Наполеон", "Настоящее масло", 'link to image', 1),
+  ("Торт Чизкейк", "С сыром Маскарпоне", 'link to image', 1),
+  ("Торт Сметанник", "Только самая свежая сметана", 'link to image', 1),
+  ("Хлеб Бородинский", "ржаной", 'link to image', 3),
+  ("Батон Семейный", "пышное тесто", 'link to image', 3),
+  ("Хлеб Мариинский", "с изюмом и кориандром", 'link to image', 3);
+"""
+
+#Создаем 2 таблицы в БД, Категории и Продукты:
+execute_query(connection, create_categories_table)  
+execute_query(connection, create_products_table)  
+
+#Создаем в таблицах записи:
+execute_query(connection, create_categories) 
+execute_query(connection, create_products)  
+
+#Функция для извлечения данных из БД
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
